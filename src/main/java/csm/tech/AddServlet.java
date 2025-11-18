d759740132dba55776fe5db7a587328404229a30
Nderitu-Dennis <nderitudenis87@gmail.com>
2025-11-18 14:22:36 +0300

    add SquareServlet, passing data btwn servlets using RequestDispatcher

--- src/main/java/csm/tech/AddServlet.java
+++ src/main/java/csm/tech/AddServlet.java
@@ -3,6 +3,8 @@ package csm.tech;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +13,7 @@ public class AddServlet extends HttpServlet {
 
 	// service method
 //	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
 //		get values from the user
 //		parse the string values from user to int
@@ -19,10 +21,18 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num2"));
 
 		int result = i + j;
+		//System.out.println(result);
+
+		// get the response frm server to client
+		 // sharing data btwn servlets-put some data in the request object
+		req.setAttribute("result", result);	
+		req.setAttribute("i", i);
+		req.setAttribute("j", j);
 		
-		//get the response frm server to client
-		PrintWriter out = res.getWriter();
-		out.println("result: "+ result);
+
+		// redirect this output to another servlet
+		RequestDispatcher rd = req.getRequestDispatcher("square"); //square is the path/url
+		rd.forward(req, res);
 
 	}
 
---
+++ src/main/java/csm/tech/SquareServlet.java
@@ -0,0 +1,24 @@
+package csm.tech;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+	// create a get method -will send a request
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		// fetching the attributes from AddServlet
+		int result = (int) req.getAttribute("result");
+		int i = (int) req.getAttribute("i");
+		int j = (int) req.getAttribute("j");
+
+		result = result * result;
+		PrintWriter out = res.getWriter();
+		out.println("square of " + i + " and " + j + " is: " + result);
+
+	}
+}
