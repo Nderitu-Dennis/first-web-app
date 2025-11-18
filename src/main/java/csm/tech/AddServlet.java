8949f3d60f2c0a04be522a5326eaa2398544ad44
Nderitu-Dennis <nderitudenis87@gmail.com>
2025-11-18 11:33:27 +0300

    add first method

--- src/main/java/csm/tech/AddServlet.java
+++ src/main/java/csm/tech/AddServlet.java
@@ -1,7 +1,27 @@
 package csm.tech;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class AddServlet extends HttpServlet{
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	// service method
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		get values from the user
+//		parse the string values from user to int
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int result = i + j;
+		
+		//get the response frm server to client
+		PrintWriter out = res.getWriter();
+		out.println("result: "+ result);
+
+	}
 
 }
